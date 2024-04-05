aee90e9f50e1c0f8eaffd11d8eefc0964bb2cfc9
Bishal <138358353+bishal520@users.noreply.github.com>
2024-04-05 11:33:39 +0545

    d

--- comment.js
+++ comment.js
@@ -1,2 +1,7 @@
 // Create web server
+onst http = require('http');
+
+const server = http.createServer((req, res) => { res.statusCode = 200; res.setHeader('Content-Type', 'text/plain'); res.end('Hello, world!'); });
+
+server.listen(3000, () => { console.log('Server is running on port 3000'); });
 // Create a web server that listens on port 3000 and serves the following responses:
